87d3f7d2db3a1ec89b4e6411f390dd95ebb51b71
farisabqari <127277279+farisabqari@users.noreply.github.com>
2023-03-25 18:30:19 +0800

    masukan void

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -53,19 +53,3 @@ void insertionsort() {
 		}
 	}
 }
-void display() {
-	cout << "\n================================" << endl;
-	cout << "Element Array Yang Telah Tersusun" << endl;
-	cout << "================================" << endl;
-
-	for (int j = 0; j < n; j++) {
-		cout << arr[j] << " ";
-	}
-}
-
-int main()
-{
-	input();
-	insertionsort();
-	display();
-}

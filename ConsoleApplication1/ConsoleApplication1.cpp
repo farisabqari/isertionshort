cdf4a25932e845d0d737e4bd131aeca008a3d45a
farisabqari <127277279+farisabqari@users.noreply.github.com>
2023-03-25 18:40:13 +0800

    memasukan void display

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -53,3 +53,19 @@ void insertionsort() {
 		}
 	}
 }
+void display() {
+	cout << "\n================================" << endl;
+	cout << "Element Array Yang Telah Tersusun" << endl;
+	cout << "================================" << endl;
+
+	for (int j = 0; j < n; j++) {
+		cout << arr[j] << " ";
+	}
+}
+
+int main()
+{
+	input();
+	insertionsort();
+	display();
+}
